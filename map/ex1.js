00fca2a20fccc33de1e9bf0d3873700974fc11dd
pierrekzo <pierrecasaux@gmail.com>
2019-10-02 18:52:13 +0200

    exo1 map over

--- map/ex1.js
+++ map/ex1.js
@@ -1,10 +1,8 @@
 /* Array.prototype.map - Exercice 1
-
 Ecrire une fonction getStringsLength qui reçoit comme un argument un tableau de chaînes.
 En utilisant map, cette fonction doit renvoyer un tableau avec,
 pour chaque chaîne CHAINE de longueur N en entrée, la chaîne "CHAINE contains N characters"
 en sortie.
-
 Exemple d'entrée:
   [
     'Chicken',
@@ -18,10 +16,12 @@ En sortie: [
   'Tofu contains 4 characters',
   'Mayonnaise contains 10 characters'
 ]
-
  */
-
 function getStringsLength(strings) {
+  const aze = strings.map(chaine =>
+    `${chaine} contains ${chaine.length} characters`);
+
+  return aze;
 }
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
