26937904b84a450ddd71919566ceaa14835b2df1
pierrekzo <pierrecasaux@gmail.com>
2019-10-02 16:25:04 +0200

    exo 2 torché

--- filter/ex2.js
+++ filter/ex2.js
@@ -2,12 +2,9 @@
 
 Ecrire une fonction keepStarks, qui prend en arguments un tableau de chaînes
 (plus spécifiquement, des noms de personnages de Game of Thrones).
-
 La fonction doit renvoyer un tableau ne contenant que les membres de la famille Stark.
-
 Il y a plusieurs façons d'écrire cette fonction, et outre filter, tu auras probablement
 besoin d'une de ces méthodes de String: endsWith ou split.
-
 Exemple d'entrée:
   [
     'Bran Stark',
@@ -17,13 +14,17 @@ Exemple d'entrée:
     'Yara Greyjoy',
     'Sansa Stark'
   ]
-
 Sortie attendue:
   ['Bran Stark', 'Arya Stark', 'Sansa Stark']
-
  */
 
 function keepStarks(names) {
+  const wtf = names.filter(name => {
+    if (name.endsWith('Stark')) {
+      return name;
+    };
+  });
+  return wtf;
 }
 
 // Ne pas modifier l'export
